9e181a960e807f40e6252336280341f76b08ddab
Ty Rawls <tyrell.rawls@gmail.com>
2024-06-07 22:30:48 -0400

    update sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'A.M.Y.T.H.E.S.T: Healthcare AI | Patient Education Chatbot'
+project = 'A.M.Y.T.H.E.S.T | Patient Education Chatbot'
 copyright = '2024, Ty Rawls'
 author = 'Ty Rawls'
 
