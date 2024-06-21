79dce7c6436d085cb4e9c8ed4e3dbca03572bf71
Ty Rawls <tyrell.rawls@gmail.com>
2024-06-20 21:00:13 -0400

    add sidebar caption

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ st.sidebar.caption(
         <div style='display: flex; align-items: center;'>
             <a href = 'https://www.linkedin.com/in/tyrellrawls/'><img src='{linkedin}' style='width: 35px; height: 35px; margin-right: 20px;'></a>
             <a href = 'mailto:tyrell.rawls@gmail.com'><img src='{email}' style='width: 28px; height: 28px; margin-right: 25px;'></a>
-            <a href = 'https://tyrawls.medium.com/'><img src='{medium}' style='width: 76px; height: 38px; margin-right: 25px;'></a>            
+            <a href = 'https://tyrawls.medium.com/'><img src='{medium}' style='width: 28px; height: 28px; margin-right: 25px;'></a>            
         </div>       
         """,
     unsafe_allow_html=True,
